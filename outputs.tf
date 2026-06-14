a2e8e31765bcfd9549873a63d1312e0b2cb3989a
rioto.nagano <kingdom950927@gmail.com>
2026-06-14 09:18:40 +0900

    add: ディストリビューションIDを出力

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "cloudfront_url" {
 output "s3_bucket_name" {
   value = aws_s3_bucket.my-company-dev-123456.bucket
 }
+
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.cdn.id
+}
